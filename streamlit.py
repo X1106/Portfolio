0ad3b5ffd7adff51870a827f3e18fafc035ae47e
X1106 <90523009+X1106@users.noreply.github.com>
2022-05-05 16:47:11 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -77,7 +77,10 @@ st.subheader('資料2.データ取り込みからデータの可視化')
 import os
 file = os.path.abspath("./data/ごみ排出量全国.csv")
 garbage = pd.read_csv(file)
-west_garbage = pd.read_csv('data/ごみ排出量西日本.csv')
+
+file2 = os.path.abspath("./data/ごみ排出量西日本.csv")
+west_garbage = pd.read_csv(file2)
+
 lat_lon= pd.read_csv('data/緯度_経度.csv')
 east_garbage = pd.read_csv('data/ごみ排出量＿東日本.csv')
 
